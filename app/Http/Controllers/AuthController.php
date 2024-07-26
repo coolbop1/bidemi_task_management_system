b9f1256029ca0f86182156557c9582d45f85293c
Bidemi Oritunmise <bidemi@bidemi@seamlesshr.com>
2024-07-26 15:03:45 +0100

    Add laravel sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+    
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+        if ($validator->fails()) {
+            $responseArr['message'] = $validator->errors();;
+            $responseArr['token'] = '';
+            return response()->json($responseArr, Response::HTTP_BAD_REQUEST);
+        }
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+            'message' => 'Invalid login details'
+                    ], 401);
+                }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+        'message' => 'Successfully logged out'
+        ]);
+
+    }
+
+    public function profile(Request $request)
+    {
+        return response()->json([
+            'data' => $request->user(),
+        ]);
+    }
+}
+
